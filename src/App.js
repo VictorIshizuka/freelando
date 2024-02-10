15398d20baf7835b94c09b7cdb722cfefdb016ce
victor rosario ishizuka <victorishizuka131@gmail.com>
2024-02-10 13:33:18 -0300

    feat: fonts and library added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Styles } from "./components/GlobalStyles/Styles";
+
 function App() {
-  return <div className="App">oi</div>;
+  return (
+    <div>
+      <Styles />
+      <h1>Freelando</h1>
+    </div>
+  );
 }
 
 export default App;
