eeaa5cc65feacbf23df761f6c37ebd9d3f36bfb6
victor rosario ishizuka <victorishizuka131@gmail.com>
2024-02-12 10:56:13 -0300

    feat: created global styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { Card } from "./common/Card/Card";
-import { Styles } from "./components/GlobalStyles/Styles";
+import { IsThemeProvider } from "./common/ThemeProvider/ThemeProvider";
+import { Styles } from "./common/GlobalStyles/Styles";
+import { Card } from "./common/components/Card/index.jsx";
 
 function App() {
   return (
-    <div>
+    <IsThemeProvider>
       <Styles />
       <Card>
         <h1>Freelando</h1>
       </Card>
-    </div>
+    </IsThemeProvider>
   );
 }
 
---
+++ src/common/ThemeProvider/ThemeProvider.jsx
@@ -0,0 +1,25 @@
+import { ThemeProvider } from "@emotion/react";
+
+const theme = {
+  colors: {
+    primary: {
+      a: "#5754eD",
+      b: "",
+      c: "",
+    },
+    secondary: {
+      a: "#EBEAF9",
+      b: "",
+      c: "",
+    },
+    neutral: {},
+  },
+  gaps: {
+    s: "16px",
+    l: "32px",
+  },
+  fontFamily: "'Montserrat', 'sans-serif'",
+};
+export const IsThemeProvider = ({ children }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
